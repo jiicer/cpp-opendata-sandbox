d9713aa53790fde6e20bca1827d5fecc079964a7
jiicer <jiis@Jaakko-Sertamo-iMac.local>
2018-07-15 23:53:58 +0300

    Conan deployed as dependency/package manager

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <sstream>
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Options.hpp>
+#include <nlohmann/json.hpp>
 
 int main() {
+    nlohmann::json json;
     std::cout << curlpp::options::Url(std::string("http://data.itsfactory.fi/journeys/api/1/vehicle-activity"));
     return 1;
 }
