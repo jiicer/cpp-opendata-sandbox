2613f220b719bc405e52128dd75bbec1d6eabe4a
jiicer <jaakko.sertamo@gmail.com>
2018-07-16 22:41:32 +0300

    JSON parsing

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
-#include <sstream>
+#include <string>
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Options.hpp>
 #include <nlohmann/json.hpp>
 
-int main() {
-    std::stringstream s;
-    s << curlpp::options::Url(std::string("http://data.itsfactory.fi/journeys/api/1/vehicle-activity"));
-    nlohmann::json json = nlohmann::json::parse(s.str());
-    std::cout << std::setw(4) << json;
-    return 1;
+using json = nlohmann::json;
+
+json journeys_by_line(const json &j, int line)
+{
+    json rv;
+    for (const auto journey : j["body"])
+    {
+         if (std::stoi(journey["monitoredVehicleJourney"]["lineRef"].get<std::string>()) == line)
+            rv.push_back(journey);
+    }
+    return rv;
 }
 
+int main()
+{
+    std::stringstream s;
+    s << curlpp::options::Url(std::string("http://data.itsfactory.fi/journeys/api/1/vehicle-activity"));
+    json json = nlohmann::json::parse(s.str());
+    std::cout << std::setw(4) << journeys_by_line(json, 3);
+    return 1;
+}
